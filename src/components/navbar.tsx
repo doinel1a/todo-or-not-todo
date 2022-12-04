3d5e2f6cda33d8e4905e8c088463d85a60b14da7
doinel1a <doinel1atanasiu@gmail.com>
2022-12-04 10:01:25 +0100

    Navbar implemented

---
+++ src/components/navbar.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Navbar() {
+	return (
+		<nav className='w-full relative top-0 p-4'>
+			<p className='text-2xl text-center'>todo or !todo</p>
+		</nav>
+	);
+}
