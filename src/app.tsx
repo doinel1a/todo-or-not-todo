3031ea4d92889367cc10ed436746de4b9529c723
doinel1a <doinel1atanasiu@gmail.com>
2022-12-04 10:06:22 +0100

    Redux configured

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+
+import Navbar from './components/navbar';
+import store from './store/store';
 
 export default function App() {
 	return (
-		<main className='h-screen flex flex-col justify-center items-center text-color bg-primary'>
-			<h1 className='text-2xl'>todo or !todo</h1>
-		</main>
+		<React.StrictMode>
+			<Provider store={store}>
+				<div className='w-screen h-screen absolute flex flex-col items-center text-color bg-primary'>
+					<Navbar />
+					<main className='w-full max-w-xl max-h-full relative flex flex-col items-center my-4 p-4 rounded-lg bg-secondary overflow-hidden'></main>
+				</div>
+			</Provider>
+		</React.StrictMode>
 	);
 }
